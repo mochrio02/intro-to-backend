4a3d31ab34d40a1e5b09086fde621d4a1de927f5
mochrio02 <mochamadvario1945@gmail.com>
2026-03-25 10:54:54 +0700

    Add controller for user register

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,49 @@
+import { User } from "../models/user.model.js";
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Basic validation
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are important!",
+      });
+    }
+
+    // Check if user already exists
+    const existing = await User.findOne({
+      email: email.toLowerCase(),
+    });
+
+    if (existing) {
+      return res.status(400).json({
+        message: "User already exists!",
+      });
+    }
+
+    // Create user
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      password,
+      loggedIn: false,
+    });
+
+    res.status(201).json({
+      message: "User registered",
+      user: {
+        id: user._id,
+        email: user.email,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export { registerUser };
